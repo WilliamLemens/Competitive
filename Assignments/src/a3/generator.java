3bae3e1ea01bcea64097227d19ca5869926e51e2
William Lemens <william.lemens@gmail.com>
2018-02-22 13:45:53 -0600

    Did CodeForces 4

---
+++ Assignments/src/a3/generator.java
@@ -0,0 +1,10 @@
+package a3;
+
+public class generator {
+	public static void main(String[] args) {
+		System.out.println(1+"\n"+1000+" "+999+" "+-100000);
+		for (int k = 1; k < 1000; k++) {
+			System.out.println(k+" "+(k+1)+" "+1);
+		}
+	}
+}
---
+++ Assignments/src/a4/p706B.java
@@ -0,0 +1,9 @@
+package a4;
+
+public class p706B {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
