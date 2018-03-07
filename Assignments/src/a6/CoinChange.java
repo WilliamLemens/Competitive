43cfa2172d56f19fe5d9d4205db51dffa13b4cca
William Lemens <william.lemens@gmail.com>
2018-03-07 16:53:31 -0600

    Work on a6

---
+++ Assignments/src/a6/CoinChange.java
@@ -0,0 +1,19 @@
+package a6;
+
+import java.util.Scanner;
+
+public class CoinChange {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t = 0; t < T; t++) {
+			int X = sc.nextInt();
+			
+			int[] f = new int[X];
+			
+			
+		}
+		sc.close();
+	}
+}
