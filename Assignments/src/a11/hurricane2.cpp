90451134fffa8b66c0ed5fe55e8b94ff8417f3ab
William Lemens <william.lemens@gmail.com>
2018-04-30 16:30:56 -0500

    Done with a11 vanilla

--- Assignments/src/a11/hurricane2.cpp
+++ Assignments/src/a11/hurricane2.cpp
@@ -4,85 +4,60 @@ using namespace std;
 
 const int MAX_N = 101;
 
-bool bfs(int graph[MAX_N][MAX_N], int N, int s, int t, int parent[]) {
-    // Create a visited array and mark all vertices as not visited
+bool BFS(int graph[MAX_N][MAX_N], int N, int s, int t, int parent[]) {
     bool visited[N];
     memset(visited, 0, sizeof(visited));
- 
-    // Create a queue, enqueue source vertex and mark source vertex
-    // as visited
+
     queue <int> q;
     q.push(s);
     visited[s] = true;
     parent[s] = -1;
- 
-    // Standard BFS Loop
-    while (!q.empty())
-    {
+
+    while (!q.empty()) {
         int u = q.front();
         q.pop();
- 
+
         for (int v = 0; v < N; v++)
-        {
-            if (!visited[v] && graph[u][v] > 0)
-            {
+            if (!visited[v] && graph[u][v] > 0) {
                 q.push(v);
                 parent[v] = u;
                 visited[v] = true;
             }
-        }
     }
- 
-    // If we reached sink in BFS starting from source, then return
-    // true, else false
-    return (visited[t] == true);
+
+    return visited[t];
 }
- 
+
 // Returns the maximum flow from s to t in the given graph
-int ford_fulkerson(int graph[MAX_N][MAX_N], int s, int t) {
+int ford_fulkerson(int G[MAX_N][MAX_N], int N, int s, int t) {
     int u, v;
- 
-    // Create a residual graph and fill the residual graph with
-    // given capacities in the original graph as residual capacities
-    // in residual graph
-    int rGraph[V][V]; // Residual graph where rGraph[i][j] indicates 
-                     // residual capacity of edge from i to j (if there
-                     // is an edge. If rGraph[i][j] is 0, then there is not)  
-    for (u = 0; u < V; u++)
-        for (v = 0; v < V; v++)
-             rGraph[u][v] = graph[u][v];
- 
-    int parent[V];  // This array is filled by BFS and to store path
- 
-    int max_flow = 0;  // There is no flow initially
- 
-    // Augment the flow while tere is path from source to sink
-    while (bfs(rGraph, s, t, parent))
-    {
-        // Find minimum residual capacity of the edges along the
-        // path filled by BFS. Or we can say find the maximum flow
-        // through the path found.
+
+    int R[MAX_N][MAX_N]; // Set up residual graph
+    for (u = 0; u < N; u++)
+        for (v = 0; v < N; v++)
+             R[u][v] = G[u][v];
+
+    int parent[N];
+    int max_flow = 0;
+
+    while (BFS(R, N, s, t, parent)) {
         int path_flow = INT_MAX;
-        for (v=t; v!=s; v=parent[v])
-        {
+
+        for (v = t; v != s; v = parent[v]) {
             u = parent[v];
-            path_flow = min(path_flow, rGraph[u][v]);
+            path_flow = min(path_flow, R[u][v]);
         }
- 
-        // update residual capacities of the edges and reverse edges
-        // along the path
-        for (v=t; v != s; v=parent[v])
-        {
+
+        // update residual capacities of the edges and reverse edges along the path
+        for (v = t; v != s; v = parent[v]) {
             u = parent[v];
-            rGraph[u][v] -= path_flow;
-            rGraph[v][u] += path_flow;
+            R[u][v] -= path_flow;
+            R[v][u] += path_flow;
         }
- 
-        // Add path flow to overall flow
+
         max_flow += path_flow;
     }
- 
-    // Return the overall flow
+
     return max_flow;
 }
 
@@ -94,7 +69,7 @@ int main() {
     for (int t = 0; t < T; t++) {
         // READ IN
         scanf("%d %d %d %d", &N, &M, &S, &P);
-        int G[N][N];
+        int G[MAX_N][MAX_N];
         for (int m = 0; m < M; m++) {
             scanf("%d %d %d", &u, &v, &c);
             G[u][v] = c;
@@ -103,22 +78,13 @@ int main() {
             scanf("%d", &H);
             G[H][0] = INT_MAX;
         }
-        /*
-        // CHECK READ
-        printf("%d %d %d %d %d\n", N, M, S, P, H);
-        for (int n = 0; n < N+1; n++) {
-            printf("%d:\n", n);
-            for (int i = 0; i < G.V[n].E.size(); i++)
-                printf("    %d %d %d\n", G.V[n].E[i].u, G.V[n].E[i].v, G.V[n].E[i].c);
-        }
-        */
-        // FEED TO EDMONDS-KARP'S
-        int maxSaved = edmonds_karp(G, N, S, 0);
+        // FEED TO FORD-FULKERSON'S
+        int maxSaved = ford_fulkerson(G, N+1, S, 0);
         // OUTPUT
         if (maxSaved >= P)
             printf("YES\n");
         else
-            printf("NO %d\n", maxSaved);
+            printf("NO %d\n", P-maxSaved);
     }
 
     return 0;
